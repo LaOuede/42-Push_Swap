444f5f40a54e523dbc8fc511f21ff8447eaa5cc8
Gwenola LE ROUX <gwenolaleroux@iMac-de-Gwenola.local>
2023-02-15 13:23:04 -0500

    swap modified parsing

--- src/swap.c
+++ src/swap.c
@@ -6,7 +6,7 @@
 /*   By: gwenolaleroux <gwenolaleroux@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 08:56:22 by gle-roux          #+#    #+#             */
-/*   Updated: 2023/02/15 09:44:51 by gwenolalero      ###   ########.fr       */
+/*   Updated: 2023/02/15 13:21:10 by gwenolalero      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@ void	ft_swap(t_stack **stack, char *move)
 {
 	int	swap;
 
-/* 	if ((*stack)->size_total < 2)
-		return ; */
+	if (!stack || (*stack)->next == NULL)
+		return ;
 	swap = (*stack)->number;
 	(*stack)->number = (*stack)->next->number;
 	(*stack)->next->number = swap;
