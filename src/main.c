8723a83a7ff881202bcb2184d5f8fd722b716e2f
Gwenola LE ROUX <gwenolaleroux@iMac-de-Gwenola.local>
2023-02-16 16:57:38 -0500

    argc < 2 ok

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gle-roux <gle-roux@student.42.fr>          +#+  +:+       +#+        */
+/*   By: gwenolaleroux <gwenolaleroux@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/06 07:44:26 by gle-roux          #+#    #+#             */
-/*   Updated: 2023/02/16 15:36:34 by gle-roux         ###   ########.fr       */
+/*   Updated: 2023/02/16 16:56:14 by gwenolalero      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,35 +70,33 @@ int	main(int argc, char **argv)
 	t_lst	*tmp;
 	t_stack	*stack;
 
-	if (argc < 2)
+	if (argc > 2)
 	{
-		write(STDERR_FILENO, "Error\n", 6);
-		exit(EXIT_FAILURE);
+		stack = ft_parse(argc, argv);
+		printf("\nNumbers to sort = %d\n", stack->size_total);
+		ft_index(stack->a, stack->size_total);
+		printf("\nList of movements =\n");
+		ft_sort(stack);
+		tmp = stack->a;
+		printf("\nStack_a = ");
+		while (tmp != NULL)
+		{
+			printf("%d ", tmp->number);
+			tmp = tmp->next;
+		}
+		printf("\n");
+		printf("\nStack_b = ");
+		tmp = stack->b;
+		while (tmp != NULL)
+		{
+			printf("%d ", tmp->number);
+			tmp = tmp->next;
+		}
+		printf("\n\n>>>>> List sorted successfuly!✅ <<<<<\n\n");
+		ft_free_lst(&stack->a);
+		ft_free_lst(&stack->a);
+		ft_free_stack(&stack);
 	}
-	stack = ft_parse(argc, argv);
-	printf("\nNumbers to sort = %d\n", stack->size_total);
-	ft_index(stack->a, stack->size_total);
-	printf("\nList of movements =\n");
-	ft_sort(stack);
-	tmp = stack->a;
-	printf("\nStack_a = ");
-	while (tmp != NULL)
-	{
-		printf("%d ", tmp->number);
-		tmp = tmp->next;
-	}
-	printf("\n");
-	printf("\nStack_b = ");
-	tmp = stack->b;
-	while (tmp != NULL)
-	{
-		printf("%d ", tmp->number);
-		tmp = tmp->next;
-	}
-	printf("\n\n>>>>> List sorted successfuly!✅ <<<<<\n\n");
-	ft_free_lst(&stack->a);
-	ft_free_lst(&stack->a);
-	ft_free_stack(&stack);
 	return (0);
 }
 
